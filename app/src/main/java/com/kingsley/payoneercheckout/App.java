84950bd847d3ed64b01b6a9aad3ee0b2d83a06a4
kingsleynwankwo <kingsley@flutterwavego.com>
2021-11-21 17:18:16 +0100

    Add application file

---
+++ app/src/main/java/com/kingsley/payoneercheckout/App.java
@@ -0,0 +1,10 @@
+package com.kingsley.payoneercheckout;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class App extends Application {
+
+}
